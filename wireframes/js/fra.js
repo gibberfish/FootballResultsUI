0ee5a3547b14aeb58fd9c9912fae5cef63e9bc44
themindbadger <themindbadger@gmail.com>
2017-09-17 19:10:37 +0100

    Added some JS to switch between date analysis panels

--- wireframes/js/fra.js
+++ wireframes/js/fra.js
@@ -23,4 +23,22 @@ $(document).ready(function() {
     $(aId + ' .panel-collapse.in').collapse('hide');
   }
 
+
+
+
+
+  $('#league-table-button').on('click', function (e) {
+    $('#league-table-button').addClass('active');
+    $('#fixtures-button').removeClass('active');
+    $('#league-table').removeClass('collapse');
+    $('#fixtures').addClass('collapse');
+  })
+
+  $('#fixtures-button').on('click', function (e) {
+    $('#league-table-button').removeClass('active');
+    $('#fixtures-button').addClass('active');
+    $('#league-table').addClass('collapse');
+    $('#fixtures').removeClass('collapse');
+  })
+
 });
